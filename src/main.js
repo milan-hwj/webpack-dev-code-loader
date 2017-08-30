fd872f5c1a656327da162e3c20cd615744cd3923
Hwj <wenjun.he@husor.com>
2017-08-30 14:30:01 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
   * @describe webpack dev-mock loaders
   * ...
   */
-module.exports = function (content) {
-    var contents = content.split('\n');
+module.exports = function (content, stat) {
+    if(!stat.sourcesContent || !stat.sourcesContent[0]) {
+        return content;
+    }
+    var contents = obj.sourcesContent[0].split('\n');
     var isReplaceLine, replaceCode;
     var regx = /[ ]*\/\/[ ]*devCode[ ]*/;
 
